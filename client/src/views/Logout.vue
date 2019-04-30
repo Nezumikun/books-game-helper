1fc32eeb745abb9e7acbfa6ce3078b544674e70e
Alexey Kozhevnikov <alexey.kozhevnikov@gmail.com>
2019-04-30 11:56:20 +0300

    Исправлено форматирование

--- client/src/views/Logout.vue
+++ client/src/views/Logout.vue
@@ -7,7 +7,7 @@ import { AUTH_LOGOUT } from '../store/actions/auth'
 
 export default {
   name: 'logout',
-  created() {
+  created () {
     this.$store.dispatch(AUTH_LOGOUT)
       .then(() => {
         this.$router.push('/login')
